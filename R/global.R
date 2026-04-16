d0d1e8a1329c870b474aaf220919e6e836024ff6
Van Tam Nguyen <nguyenta>
2026-04-16 14:46:34 +0200

    Update global.R

--- R/global.R
+++ R/global.R
@@ -22,35 +22,50 @@ library(trend)
 library(plotly)
 library(randomForest)
 
-# setwd("C:/Users/nguyenta/Documents/LEILA/working_code_documentation/code/leila_visualization")
+#setwd("C:/Users/nguyenta/Documents/LEILA/working_code_documentation/code/leila_visualization")
 
 # Time series data
 timeseries_camels <- "data/CAMELS_DE_hydromet_timeseries_combine.csv"
 
-# Read catchment attributes and hydrological indicators, stations, catchments
+# Read catchment attributes and hydrological indicators
 att_hydro  <- read_csv("data/attributes_and_hydrological_indicator.csv", 
-                       show_col_types = FALSE) 
-stations    <- st_read("data/CAMELS_DE_gauging_stations.shp")
-catchments  <- st_read("data/CAMELS_DE_catchments.shp")
+                       show_col_types = FALSE)
 
-# Schutzgebiete, Nitratbelastete Gebiete, Hydrogeologische Einheiten
-schutzgetbiet <- st_transform(st_read("data/schutzgebiet"), 4326)
-nitratbelastete_gebiete <- st_read("data/Nitratbelastete_Gebiete.shp")
+# Read station shape file
+stations <- st_read("data/CAMELS_DE_gauging_stations.shp", 
+                       quiet = TRUE)
+
+# Read catchment shape file
+catchments  <- st_read("data/CAMELS_DE_catchments.shp",
+                       quiet = TRUE)
+
+# Read nature projection area shpe file
+schutzgetbiet <- st_transform(st_read("data/schutzgebiet", quiet = TRUE), 4326)
+
+# Read nitrate polluted area shape file
+nitratbelastete_gebiete <- st_read("data/Nitratbelastete_Gebiete.shp", 
+                                   quiet = TRUE)
+
+# Read hydrogeological map
 huek <- rast("data/huek.tif")
+
+# Read population density map
 population_density <- rast("data/population_density.tif")
 
 # Read groundwater wells
 selected_wells <- read_csv("data/selected_well_per_catchment_unique.csv", 
                            show_col_types = FALSE)
+
 gw_wells <- read.csv("data/gw_meta_monthly_geo_qc.txt", 
-                     sep = ";", header = TRUE) %>% as_tibble() %>%
-  filter(UFZ.ID %in% selected_wells$well_id)
+                     sep = ";", header = TRUE) %>% 
+  as_tibble() %>% filter(UFZ.ID %in% selected_wells$well_id)
 
 gw_wells <- st_as_sf(gw_wells, coords = c("x_EPSG25832", "y_EPSG25832"), 
                      crs = 25832, remove = FALSE)
+
 gw_wells <- st_transform(gw_wells, 4326)
 
-# Read abwassermenge
+# Read waste water discharge
 waste_water_discharge <- st_read("data/abwassermenge.geojson", quiet = TRUE)
 
 message(" Done reading all data")
